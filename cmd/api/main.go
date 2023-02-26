50e90aeaf7249eae91fcabbbf29debee712f3d51
niki-gor <nic.gor@yandex.ru>
2023-02-26 23:14:11 +0300

    Use autodocs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
 	chatrepository "project/internal/chat/repository"
 	"project/internal/middleware"
 	userrepository "project/internal/user/repository"
+
+	"github.com/gorilla/mux"
 
 	chatusecase "project/internal/chat/usecase"
 	userusecase "project/internal/user/usecase"
 
 	httpchat "project/internal/chat/delivery/http"
 	httpuser "project/internal/user/delivery/http"
+
+	_ "project/cmd/api/docs"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 )
+
+// @title Technogram
+// @version 1.0.0
+// @description Telegram wannabe
+
+// @host localhost:8081
+// @BasePath /
+
+// securityDefinitions.apikey ApikeyAuth
+// @in header
+// @name Authorization
 
 func main() {
 
@@ -28,6 +44,8 @@ func main() {
 
 	httpuser.NewUserHandler(r, userImpl)
 	httpchat.NewChatHandler(r, chatImpl)
+
+	r.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
 
 	http.ListenAndServe(":8081", r)
 }
---
+++ internal/auth/auth.go
@@ -0,0 +1,6 @@
+package auth
+
+import (
+	"net/http"
+)
+

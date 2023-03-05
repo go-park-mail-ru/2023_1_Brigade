40f670b4bac8f9c34866516b599d6e44d88ff72e
marcussss1 <danilalakalash60@gmail.com>
2023-03-06 02:52:37 +0300

    CORS

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -12,7 +12,7 @@ func RequestResponseMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		next.ServeHTTP(w, r)
 		//r.Header.Get()

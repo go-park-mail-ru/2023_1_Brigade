6f67210831870eb90f5e0c509bfe8134e36fb2d8
marcussss1 <danilalakalash60@gmail.com>
2023-03-05 10:52:18 +0300

    remove cors

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -18,14 +18,14 @@ func RequestResponseMiddleware(next http.Handler) http.Handler {
 //	})
 //}
 
-func Cors(next http.Handler) http.Handler {
-	//handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	//	log.Info(r.Header, r.Host, r.Body)
-	//	w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-	//	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers, Content-Length, User-Agent, X-CSRF-Token")
-	//	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
-	//	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	//})
-
-	return handler
-}
+//func Cors(next http.Handler) http.Handler {
+//	//handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//	//	log.Info(r.Header, r.Host, r.Body)
+//	//	w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+//	//	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers, Content-Length, User-Agent, X-CSRF-Token")
+//	//	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+//	//	w.Header().Set("Access-Control-Allow-Credentials", "true")
+//	//})
+//
+//	//return handler
+//}

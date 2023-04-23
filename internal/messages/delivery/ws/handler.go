51e273ee85a0f72cb48a3b551558c4d1eeea9067
marcussss1 <danilalakalash60@gmail.com>
2023-04-23 13:15:46 +0300

    edited centrifugo logic

--- internal/messages/delivery/ws/handler.go
+++ internal/messages/delivery/ws/handler.go
@@ -16,95 +16,35 @@ type messageHandler struct {
 	messageUsecase messages.Usecase
 	upgrader       websocket.Upgrader
 	clients        map[uint64]*websocket.Conn
+	centrifugo     *centrifuge.Client
 }
 
 func (u *messageHandler) SendMessagesHandler(ctx echo.Context) error {
-	config := centrifuge.Config{}
-	c := centrifuge.NewJsonClient("ws://centrifugo:8900/connection/websocket", config)
-	c.OnError(func(e centrifuge.ErrorEvent) {
-		log.Error(e.Error)
-	})
-	c.OnConnecting(func(e centrifuge.ConnectingEvent) {
-		log.Warn("Connecting - %d (%s)", e.Code, e.Reason)
-	})
-	c.OnConnected(func(e centrifuge.ConnectedEvent) {
-		log.Warn("Connected with ID %s", e.ClientID)
-	})
-	c.OnDisconnected(func(e centrifuge.DisconnectedEvent) {
-		log.Warn("Disconnected: %d (%s)", e.Code, e.Reason)
-	})
-
-	c.OnError(func(e centrifuge.ErrorEvent) {
-		log.Error("Error: %s", e.Error.Error())
-	})
-
-	c.OnMessage(func(e centrifuge.MessageEvent) {
-		log.Printf("Message from server: %s", string(e.Data))
-	})
-
-	c.OnSubscribed(func(e centrifuge.ServerSubscribedEvent) {
-		log.Warn("Subscribed to server-side channel %s: (was recovering: %v, recovered: %v)", e.Channel, e.WasRecovering, e.Recovered)
-	})
-	c.OnSubscribing(func(e centrifuge.ServerSubscribingEvent) {
-		log.Warn("Subscribing to server-side channel %s", e.Channel)
-	})
-	c.OnUnsubscribed(func(e centrifuge.ServerUnsubscribedEvent) {
-		log.Warn("Unsubscribed from server-side channel %s", e.Channel)
-	})
-
-	c.OnPublication(func(e centrifuge.ServerPublicationEvent) {
-		log.Warn("Publication from server-side channel %s: %s (offset %d)", e.Channel, e.Data, e.Offset)
-	})
-	c.OnJoin(func(e centrifuge.ServerJoinEvent) {
-		log.Warn("Join to server-side channel %s: %s (%s)", e.Channel, e.User, e.Client)
-	})
-	c.OnLeave(func(e centrifuge.ServerLeaveEvent) {
-		log.Warn("Leave from server-side channel %s: %s (%s)", e.Channel, e.User, e.Client)
-	})
-	defer c.Close()
-
-	err := c.Connect()
-	if err != nil {
-		log.Error(err)
-	}
-
-	sub, err := c.NewSubscription("channel", centrifuge.SubscriptionConfig{
+	sub, err := u.centrifugo.NewSubscription("channel", centrifuge.SubscriptionConfig{
 		Recoverable: true,
 		JoinLeave:   true,
 	})
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
-	sub.OnSubscribing(func(e centrifuge.SubscribingEvent) {
-		log.Warn("Subscribing on channel %s - %d (%s)", sub.Channel, e.Code, e.Reason)
-	})
-	sub.OnSubscribed(func(e centrifuge.SubscribedEvent) {
-		log.Warn("Subscribed on channel %s, (was recovering: %v, recovered: %v)", sub.Channel, e.WasRecovering, e.Recovered)
-	})
-	sub.OnUnsubscribed(func(e centrifuge.UnsubscribedEvent) {
-		log.Warn("Unsubscribed from channel %s - %d (%s)", sub.Channel, e.Code, e.Reason)
-	})
-
-	sub.OnError(func(e centrifuge.SubscriptionErrorEvent) {
-		log.Error("Subscription error %s: %s", sub.Channel, e.Error)
-	})
-
 	sub.OnPublication(func(e centrifuge.PublicationEvent) {
-		log.Warn("получил публикацию")
 		msg, err := u.messageUsecase.ReceiveMessage(ctx)
 		if err != nil {
 			log.Error(err)
+			return
 		}
 
 		var producerMessage model.ProducerMessage
 		err = json.Unmarshal(msg, &producerMessage)
 		if err != nil {
 			log.Error(err)
+			return
 		}
 
 		client := u.clients[producerMessage.ReceiverID]
 		if client == nil {
+			log.Error("nil client")
 			return
 		}
 
@@ -114,64 +54,19 @@ func (u *messageHandler) SendMessagesHandler(ctx echo.Context) error {
 		}
 	})
 
-	sub.OnJoin(func(e centrifuge.JoinEvent) {
-		log.Warn("Someone joined %s: user id %s, client id %s", sub.Channel, e.User, e.Client)
-	})
-	sub.OnLeave(func(e centrifuge.LeaveEvent) {
-		log.Warn("Someone left %s: user id %s, client id %s", sub.Channel, e.User, e.Client)
-	})
-
 	err = sub.Subscribe()
 	if err != nil {
-		log.Fatalln(err)
-	}
-
-	//res, err := sub.Publish(context.Background(), []byte{})
-	//log.Error(res, err)
-
-	ws, err := u.upgrader.Upgrade(ctx.Response(), ctx.Request(), nil)
-	if err != nil {
-		log.Error(err)
 		return err
 	}
 
+	ws, err := u.upgrader.Upgrade(ctx.Response(), ctx.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
 	session := ctx.Get("session").(model.Session)
 	u.clients[session.UserId] = ws
-
-	defer ws.Close()
-	//defer func() {
-	//	err := ws.Close()
-	//	if err != nil {
-	//		log.Error(err)
-	//	}
-	//}()
-
-	//
-	//go func() {
-	//	for {
-	//		msg, err := u.messageUsecase.ReceiveMessage(ctx)
-	//		if err != nil {
-	//			log.Error(err)
-	//		}
-	//
-	//		var producerMessage model.ProducerMessage
-	//		err = json.Unmarshal(msg, &producerMessage)
-	//		if err != nil {
-	//			log.Error(err)
-	//		}
-	//
-	//		client := u.clients[producerMessage.ReceiverID]
-	//		if client == nil {
-	//			continue
-	//		}
-	//
-	//		err = client.WriteMessage(websocket.TextMessage, msg)
-	//		if err != nil {
-	//			log.Error(err)
-	//			continue
-	//		}
-	//	}
-	//}()
 
 	for {
 		_, message, err := ws.ReadMessage()
@@ -187,6 +82,15 @@ func (u *messageHandler) SendMessagesHandler(ctx echo.Context) error {
 }
 
 func NewMessagesHandler(e *echo.Echo, messageUsecase messages.Usecase) messageHandler {
+	config := centrifuge.Config{}
+	c := centrifuge.NewJsonClient("ws://centrifugo:8900/connection/websocket", config)
+	defer c.Close()
+
+	err := c.Connect()
+	if err != nil {
+		log.Error(err)
+	}
+
 	handler := messageHandler{
 		messageUsecase: messageUsecase,
 		upgrader: websocket.Upgrader{
@@ -197,7 +101,8 @@ func NewMessagesHandler(e *echo.Echo, messageUsecase messages.Usecase) messageHa
 			},
 			HandshakeTimeout: time.Second * 3600,
 		},
-		clients: make(map[uint64]*websocket.Conn),
+		clients:    make(map[uint64]*websocket.Conn),
+		centrifugo: c,
 	}
 
 	sendMessagesUrl := "/message/"

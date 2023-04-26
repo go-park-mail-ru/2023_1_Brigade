f41e2e8f3093aa157aa1d9dedf6ccd87104c6b1e
marcussss1 <danilalakalash60@gmail.com>
2023-04-27 01:43:57 +0300

    edited messages logic

--- internal/messages/usecase/usecase.go
+++ internal/messages/usecase/usecase.go
@@ -17,7 +17,6 @@ import (
 	consumerUsecase "project/internal/qaas/send_messages/consumer/usecase"
 	"project/internal/qaas/send_messages/producer"
 	producerUsecase "project/internal/qaas/send_messages/producer/usecase"
-	"time"
 )
 
 type usecase struct {
@@ -83,24 +82,82 @@ func (u usecase) centrifugePublication(jsonWebSocketMessage []byte) error {
 }
 
 func (u usecase) SwitchMessageType(ctx context.Context, jsonWebSocketMessage []byte) error {
+
 	var webSocketMessage model.WebSocketMessage
 	err := json.Unmarshal(jsonWebSocketMessage, &webSocketMessage)
 	if err != nil {
 		return err
 	}
 
-	switch webSocketMessage.Type {
-	case configs.Create:
-		return u.PutInProducer(ctx, model.ProducerMessage{
-			Id:       webSocketMessage.Id,
-			Type:     webSocketMessage.Type,
-			Body:     webSocketMessage.Body,
-			AuthorId: webSocketMessage.AuthorID,
-			ChatID:   webSocketMessage.ChatID,
-		})
+	id := webSocketMessage.Id
+	//createdAt := time.Now()
+
+	// если пришел ивент на создание сообщения (0)
+	if id == "" {
+		id = uuid.New().String()
 	}
 
-	return errors.New("не выбран ни один из трех 0, 1, 2")
+	producerMessage := model.ProducerMessage{
+		Id:       id,
+		Type:     webSocketMessage.Type,
+		Body:     webSocketMessage.Body,
+		AuthorId: webSocketMessage.AuthorID,
+		ChatID:   webSocketMessage.ChatID,
+		//CreatedAt: createdAt,
+	}
+
+	switch producerMessage.Type {
+	case configs.Create:
+		go func() {
+			_, err = u.messagesRepo.InsertMessageInDB(ctx, model.Message{
+				Id:       id,
+				Body:     producerMessage.Body,
+				AuthorId: producerMessage.AuthorId,
+				ChatId:   producerMessage.ChatID,
+				//CreatedAt: createdAt,
+			})
+			if err != nil {
+				log.Error(err)
+			}
+		}()
+	case configs.Edit:
+		go func() {
+			_, err = u.messagesRepo.EditMessageById(ctx, producerMessage)
+			if err != nil {
+				log.Error(err)
+			}
+		}()
+	case configs.Delete:
+		go func() {
+			err = u.messagesRepo.DeleteMessageById(ctx, id)
+			if err != nil {
+				log.Error(err)
+			}
+		}()
+	default:
+		return errors.New("не выбран ни один из трех 0, 1, 2")
+	}
+
+	return u.PutInProducer(ctx, producerMessage)
+
+	//var webSocketMessage model.WebSocketMessage
+	//err := json.Unmarshal(jsonWebSocketMessage, &webSocketMessage)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//switch webSocketMessage.Type {
+	//case configs.Create:
+	//	return u.PutInProducer(ctx, model.ProducerMessage{
+	//		Id:       webSocketMessage.Id,
+	//		Type:     webSocketMessage.Type,
+	//		Body:     webSocketMessage.Body,
+	//		AuthorId: webSocketMessage.AuthorID,
+	//		ChatID:   webSocketMessage.ChatID,
+	//	})
+	//}
+	//
+	//return errors.New("не выбран ни один из трех 0, 1, 2")
 }
 
 func (u usecase) PutInProducer(ctx context.Context, producerMessage model.ProducerMessage) error {
@@ -109,37 +166,11 @@ func (u usecase) PutInProducer(ctx context.Context, producerMessage model.Produc
 		return err
 	}
 
-	id := uuid.New().String()
-	createdAt := time.Now()
-
-	go func() {
-		message := model.Message{
-			Id:        id,
-			Body:      producerMessage.Body,
-			AuthorId:  producerMessage.AuthorId,
-			ChatId:    producerMessage.ChatID,
-			CreatedAt: createdAt,
-		}
-
-		_, err = u.messagesRepo.InsertMessageInDB(context.Background(), message)
-		if err != nil {
-			log.Error(err)
-		}
-	}()
-
 	for _, member := range members {
 		if member.MemberId == producerMessage.AuthorId {
 			continue
 		}
 
-		producerMessage := model.ProducerMessage{
-			Id:         id,
-			Body:       producerMessage.Body,
-			AuthorId:   producerMessage.AuthorId,
-			ChatID:     producerMessage.ChatID,
-			ReceiverID: member.MemberId,
-			CreatedAt:  createdAt,
-		}
 		jsonProducerMessage, err := json.Marshal(producerMessage)
 		if err != nil {
 			return err
@@ -150,18 +181,71 @@ func (u usecase) PutInProducer(ctx context.Context, producerMessage model.Produc
 			return err
 		}
 
-		jsonWebSocketMessage, err := json.Marshal(producerMessage)
-		if err != nil {
-			return err
-		}
-
-		err = u.centrifugePublication(jsonWebSocketMessage)
+		err = u.centrifugePublication(jsonProducerMessage)
 		if err != nil {
 			return err
 		}
 	}
 
 	return nil
+	//members, err := u.chatRepo.GetChatMembersByChatId(context.Background(), producerMessage.ChatID)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//id := uuid.New().String()
+	//createdAt := time.Now()
+	//
+	//go func() {
+	//	message := model.Message{
+	//		Id:        id,
+	//		Body:      producerMessage.Body,
+	//		AuthorId:  producerMessage.AuthorId,
+	//		ChatId:    producerMessage.ChatID,
+	//		CreatedAt: createdAt,
+	//	}
+	//
+	//	_, err = u.messagesRepo.InsertMessageInDB(context.Background(), message)
+	//	if err != nil {
+	//		log.Error(err)
+	//	}
+	//}()
+	//
+	//for _, member := range members {
+	//	if member.MemberId == producerMessage.AuthorId {
+	//		continue
+	//	}
+	//
+	//	producerMessage := model.ProducerMessage{
+	//		Id:         id,
+	//		Body:       producerMessage.Body,
+	//		AuthorId:   producerMessage.AuthorId,
+	//		ChatID:     producerMessage.ChatID,
+	//		ReceiverID: member.MemberId,
+	//		CreatedAt:  createdAt,
+	//	}
+	//	jsonProducerMessage, err := json.Marshal(producerMessage)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	err = u.producer.ProduceMessage(jsonProducerMessage)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	jsonWebSocketMessage, err := json.Marshal(producerMessage)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	err = u.centrifugePublication(jsonWebSocketMessage)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+	//
+	//return nil
 }
 
 func (u usecase) PullFromConsumer(ctx context.Context) ([]byte, error) {

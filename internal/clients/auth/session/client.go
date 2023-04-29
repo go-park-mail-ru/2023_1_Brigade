bd691d8ffb9b21bc658268c9057a73cd8c45bba8
marcussss1 <danilalakalash60@gmail.com>
2023-04-29 14:46:25 +0300

    edited makefile

---
+++ internal/clients/auth/session/client.go
@@ -0,0 +1,32 @@
+package auth
+
+//type authSessionServiceGRPCClient struct {
+//	authClient generated.AuthClient
+//}
+//
+//func NewAuthUserServiceGRPSClient(con *grpc.ClientConn) authSession.Usecase {
+//	return &authSessionServiceGRPCClient{
+//		authClient: generated.NewAuthClient(con),
+//	}
+//}
+//
+//func (as authSessionServiceGRPCClient) GetSessionByCookie(ctx context.Context, cookie string) (model.Session, error) {
+//	result, err := as.authClient.GetSessionByCookie(ctx, &generated.Cookie{
+//		Cookie: cookie,
+//	})
+//	return model_conversion.FromProtoSessionToSession(result), err
+//}
+//
+//func (as authSessionServiceGRPCClient) CreateSessionById(ctx context.Context, userID uint64) (model.Session, error) {
+//	result, err := as.authClient.CreateSessionById(ctx, &generated.UserID{
+//		UserID: userID,
+//	})
+//	return model_conversion.FromProtoSessionToSession(result), err
+//}
+//
+//func (as authSessionServiceGRPCClient) DeleteSessionByCookie(ctx context.Context, cookie string) error {
+//	_, err := as.authClient.DeleteSessionByCookie(ctx, &generated.Cookie{
+//		Cookie: cookie,
+//	})
+//	return err
+//}

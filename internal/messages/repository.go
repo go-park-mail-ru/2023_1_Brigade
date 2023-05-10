830be687da85768eccf6256e208a20f88befefd3
marcussss1 <danilalakalash60@gmail.com>
2023-05-10 23:56:55 +0300

    fixed repository bugs

--- internal/messages/repository.go
+++ internal/messages/repository.go
@@ -1,20 +1,3 @@
-//package messages
-//
-//import (
-//	"context"
-//	"project/internal/model"
-//)
-//
-//type Repository interface {
-//	DeleteMessageById(ctx context.Context, messageID string) error
-//	GetMessageById(ctx context.Context, messageID string) (model.Message, error)
-//	EditMessageById(ctx context.Context, producerMessage model.ProducerMessage) (model.Message, error)
-//	GetChatMessages(ctx context.Context, chatID uint64) ([]model.ChatMessages, error)
-//	GetLastChatMessage(ctx context.Context, chatID uint64) (model.Message, error)
-//	InsertMessageInDB(ctx context.Context, message model.Message) error
-//	GetSearchMessages(ctx context.Context, userID uint64, string string) ([]model.Message, error)
-//}
-
 package messages
 
 import (
@@ -28,6 +11,6 @@ type Repository interface {
 	EditMessageById(ctx context.Context, producerMessage model.ProducerMessage) (model.Message, error)
 	GetChatMessages(ctx context.Context, chatID uint64) ([]model.ChatMessages, error)
 	GetLastChatMessage(ctx context.Context, chatID uint64) (model.Message, error)
-	InsertMessageInDB(ctx context.Context, message model.Message) (model.Message, error)
+	InsertMessageInDB(ctx context.Context, message model.Message) error
 	GetSearchMessages(ctx context.Context, userID uint64, string string) ([]model.Message, error)
 }
--- internal/messages/repository/repository.go
+++ internal/messages/repository/repository.go
@@ -1,156 +1,3 @@
-//package repository
-//
-//import (
-//	"context"
-//	"database/sql"
-//	"errors"
-//	"github.com/jmoiron/sqlx"
-//	"project/internal/messages"
-//	"project/internal/model"
-//	myErrors "project/internal/pkg/errors"
-//)
-//
-//type repository struct {
-//	db *sqlx.DB
-//}
-//
-//func NewMessagesMemoryRepository(db *sqlx.DB) messages.Repository {
-//	return &repository{db: db}
-//}
-//
-//func (r repository) EditMessageById(ctx context.Context, producerMessage model.ProducerMessage) (model.Message, error) {
-//	var message model.Message
-//	err := r.db.GetContext(ctx, &message, "UPDATE message SET body = $1, created_at = $2 WHERE id = $3 RETURNING *", producerMessage.Body, producerMessage.CreatedAt, producerMessage.Id)
-//	if err != nil {
-//		return model.Message{}, err
-//	}
-//
-//	return message, nil
-//}
-//
-//func (r repository) DeleteMessageById(ctx context.Context, messageID string) error {
-//	tx, err := r.db.BeginTxx(ctx, nil)
-//	if err != nil {
-//		return err
-//	}
-//
-//	_, err = r.db.ExecContext(ctx, "DELETE FROM message WHERE id=$1", messageID)
-//	if err != nil {
-//		tx.Rollback()
-//		return err
-//	}
-//
-//	_, err = r.db.ExecContext(ctx, "DELETE FROM chat_messages WHERE id_message=$1", messageID)
-//	if err != nil {
-//		tx.Rollback()
-//		return err
-//	}
-//
-//	err = tx.Commit()
-//	if err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
-//
-//func (r repository) GetMessageById(ctx context.Context, messageID string) (model.Message, error) {
-//	var message model.Message
-//	err := r.db.GetContext(ctx, &message, "SELECT * FROM message WHERE id=$1", messageID)
-//
-//	if err != nil {
-//		if errors.Is(err, sql.ErrNoRows) {
-//			return model.Message{}, myErrors.ErrMessageNotFound
-//		}
-//
-//		return model.Message{}, err
-//	}
-//
-//	return message, nil
-//}
-//
-//func (r repository) GetChatMessages(ctx context.Context, chatID uint64) ([]model.ChatMessages, error) {
-//	var chatMessages []model.ChatMessages
-//	err := r.db.SelectContext(ctx, &chatMessages, "SELECT * FROM chat_messages WHERE id_chat=$1", chatID)
-//
-//	if err != nil {
-//		if errors.Is(err, sql.ErrNoRows) {
-//			return nil, myErrors.ErrMembersNotFound
-//		}
-//
-//		return nil, err
-//	}
-//
-//	return chatMessages, nil
-//}
-//
-//func (r repository) InsertMessageInDB(ctx context.Context, message model.Message) error {
-//	tx, err := r.db.BeginTxx(ctx, nil)
-//	if err != nil {
-//		return err
-//	}
-//
-//	_, err = r.db.NamedExecContext(ctx, `INSERT INTO message (id, body, id_chat, author_id, created_at) `+
-//		`VALUES (:id, :body, :id_chat, :author_id, :created_at)`, message)
-//
-//	if err != nil {
-//		tx.Rollback()
-//		return err
-//	}
-//
-//	_, err = r.db.NamedExecContext(ctx, "INSERT INTO chat_messages (id_chat, id_message) VALUES (:id_chat, :id_message)", model.ChatMessages{
-//		ChatId:    message.ChatId,
-//		MessageId: message.Id,
-//	})
-//
-//	if err != nil {
-//		tx.Rollback()
-//		return err
-//	}
-//
-//	err = tx.Commit()
-//	if err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
-//
-//func (r repository) GetLastChatMessage(ctx context.Context, chatID uint64) (model.Message, error) {
-//	var lastMessage model.Message
-//	err := r.db.Get(&lastMessage, `SELECT * FROM message WHERE id_chat = $1 AND created_at = (SELECT MAX(created_at) FROM message WHERE id_chat = $1)`, chatID)
-//
-//	if err != nil {
-//		if errors.Is(err, sql.ErrNoRows) {
-//			return model.Message{}, nil
-//		}
-//
-//		return model.Message{}, err
-//	}
-//
-//	return lastMessage, nil
-//}
-//
-//func (r repository) GetSearchMessages(ctx context.Context, userID uint64, string string) ([]model.Message, error) {
-//	var messages []model.Message
-//	err := r.db.Select(&messages, `
-//		SELECT message.*
-//		FROM message
-//		JOIN chat_messages ON message.id = chat_messages.id_message
-//		JOIN chat_members ON chat_messages.id_chat = chat_members.id_chat
-//		WHERE message.body ILIKE $1 AND chat_members.id_member = $2;`,
-//		"%"+string+"%", userID)
-//	if err != nil {
-//		if err == sql.ErrNoRows {
-//			return nil, myErrors.ErrChatNotFound
-//		}
-//
-//		return nil, err
-//	}
-//
-//	return messages, nil
-//}
-
 package repository
 
 import (
@@ -173,7 +20,7 @@ func NewMessagesMemoryRepository(db *sqlx.DB) messages.Repository {
 
 func (r repository) EditMessageById(ctx context.Context, producerMessage model.ProducerMessage) (model.Message, error) {
 	var message model.Message
-	err := r.db.Get(&message, "UPDATE message SET body = $1, created_at = $2 WHERE id = $3 RETURNING *", producerMessage.Body, producerMessage.CreatedAt, producerMessage.Id)
+	err := r.db.GetContext(ctx, &message, "UPDATE message SET body = $1, created_at = $2 WHERE id = $3 RETURNING *", producerMessage.Body, producerMessage.CreatedAt, producerMessage.Id)
 	if err != nil {
 		return model.Message{}, err
 	}
@@ -182,14 +29,26 @@ func (r repository) EditMessageById(ctx context.Context, producerMessage model.P
 }
 
 func (r repository) DeleteMessageById(ctx context.Context, messageID string) error {
-	_, err := r.db.Exec("DELETE FROM message WHERE id=$1", messageID)
-	if errors.Is(err, sql.ErrNoRows) {
-		return myErrors.ErrMessageNotFound
+	tx, err := r.db.BeginTxx(ctx, nil)
+	if err != nil {
+		return err
 	}
 
-	_, err = r.db.Exec("DELETE FROM chat_messages WHERE id_message=$1", messageID)
-	if errors.Is(err, sql.ErrNoRows) {
-		return myErrors.ErrMessageNotFound
+	_, err = r.db.ExecContext(ctx, "DELETE FROM message WHERE id=$1", messageID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = r.db.ExecContext(ctx, "DELETE FROM chat_messages WHERE id_message=$1", messageID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -197,71 +56,79 @@ func (r repository) DeleteMessageById(ctx context.Context, messageID string) err
 
 func (r repository) GetMessageById(ctx context.Context, messageID string) (model.Message, error) {
 	var message model.Message
-	err := r.db.Get(&message, "SELECT * FROM message WHERE id=$1", messageID)
-
-	if errors.Is(err, sql.ErrNoRows) {
-		return model.Message{}, myErrors.ErrMessageNotFound
-	}
-
-	return message, err
-}
-
-func (r repository) GetChatMessages(ctx context.Context, chatID uint64) ([]model.ChatMessages, error) {
-	var chatMessages []model.ChatMessages
-	rows, err := r.db.Query("SELECT * FROM chat_messages WHERE id_chat=$1", chatID)
-	defer rows.Close()
+	err := r.db.GetContext(ctx, &message, "SELECT * FROM message WHERE id=$1", messageID)
 
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return nil, myErrors.ErrChatNotFound
-		}
-		return nil, err
-	}
-
-	for rows.Next() {
-		var chatMessage model.ChatMessages
-		err := rows.Scan(&chatMessage.ChatId, &chatMessage.MessageId)
-		if err != nil {
-			return nil, err
+			return model.Message{}, myErrors.ErrMessageNotFound
 		}
 
-		chatMessages = append(chatMessages, chatMessage)
-	}
-
-	return chatMessages, err
-}
-
-func (r repository) InsertMessageInDB(ctx context.Context, message model.Message) (model.Message, error) {
-	rows, err := r.db.NamedQuery(`INSERT INTO message (id, body, id_chat, author_id, created_at) `+
-		`VALUES (:id, :body, :id_chat, :author_id, :created_at)`, message)
-	defer rows.Close()
-
-	if err != nil {
-		return model.Message{}, err
-	}
-
-	rows, err = r.db.NamedQuery("INSERT INTO chat_messages (id_chat, id_message) VALUES (:id_chat, :id_message)", model.ChatMessages{
-		ChatId:    message.ChatId,
-		MessageId: message.Id,
-	})
-	defer rows.Close()
-
-	if err != nil {
 		return model.Message{}, err
 	}
 
 	return message, nil
 }
 
+func (r repository) GetChatMessages(ctx context.Context, chatID uint64) ([]model.ChatMessages, error) {
+	var chatMessages []model.ChatMessages
+	err := r.db.SelectContext(ctx, &chatMessages, "SELECT * FROM chat_messages WHERE id_chat=$1", chatID)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, myErrors.ErrMembersNotFound
+		}
+
+		return nil, err
+	}
+
+	return chatMessages, nil
+}
+
+func (r repository) InsertMessageInDB(ctx context.Context, message model.Message) error {
+	tx, err := r.db.BeginTxx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.db.NamedExecContext(ctx, `INSERT INTO message (id, body, id_chat, author_id, created_at) `+
+		`VALUES (:id, :body, :id_chat, :author_id, :created_at)`, message)
+
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = r.db.NamedExecContext(ctx, "INSERT INTO chat_messages (id_chat, id_message) VALUES (:id_chat, :id_message)", model.ChatMessages{
+		ChatId:    message.ChatId,
+		MessageId: message.Id,
+	})
+
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (r repository) GetLastChatMessage(ctx context.Context, chatID uint64) (model.Message, error) {
 	var lastMessage model.Message
 	err := r.db.Get(&lastMessage, `SELECT * FROM message WHERE id_chat = $1 AND created_at = (SELECT MAX(created_at) FROM message WHERE id_chat = $1)`, chatID)
 
-	if errors.Is(err, sql.ErrNoRows) {
-		return model.Message{}, nil
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return model.Message{}, nil
+		}
+
+		return model.Message{}, err
 	}
 
-	return lastMessage, err
+	return lastMessage, nil
 }
 
 func (r repository) GetSearchMessages(ctx context.Context, userID uint64, string string) ([]model.Message, error) {

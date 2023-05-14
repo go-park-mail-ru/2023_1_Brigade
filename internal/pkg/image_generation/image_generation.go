4dd654e91f12fa8c9958df01a649dbbb2fbb8a19
Ubuntu <danilakalash60@gmail.com>
2023-05-14 16:33:49 +0000

    fixed bugs

--- internal/pkg/image_generation/image_generation.go
+++ internal/pkg/image_generation/image_generation.go
@@ -36,7 +36,7 @@ func GenerateAvatar(firstCharacterName string) error {
 
 	draw.Draw(img, img.Bounds(), &image.Uniform{color}, image.Point{}, draw.Src)
 
-	file, err := os.Create("../../avatars/background.png")
+	file, err := os.Create("avatars/background.png")
 	if err != nil {
 		return err
 	}
@@ -46,7 +46,7 @@ func GenerateAvatar(firstCharacterName string) error {
 	}
 
 	const S = 1024
-	im, err := gg.LoadImage("../../avatars/background.png")
+	im, err := gg.LoadImage("avatars/background.png")
 	if err != nil {
 		return err
 	}
@@ -55,7 +55,7 @@ func GenerateAvatar(firstCharacterName string) error {
 	dc.SetRGB(1, 1, 1)
 	dc.Clear()
 	dc.SetRGB(1, 1, 1)
-	if err := dc.LoadFontFace("../../avatars/Go-Mono.ttf", 728); err != nil {
+	if err := dc.LoadFontFace("avatars/Go-Mono.ttf", 728); err != nil {
 		return err
 	}
 
@@ -64,7 +64,7 @@ func GenerateAvatar(firstCharacterName string) error {
 	dc.DrawStringAnchored(firstCharacterName, S/2, S/2, 0.5, 0.5)
 	dc.Clip()
 
-	err = dc.SavePNG("../../avatars/background.png")
+	err = dc.SavePNG("avatars/background.png")
 	if err != nil {
 		return err
 	}

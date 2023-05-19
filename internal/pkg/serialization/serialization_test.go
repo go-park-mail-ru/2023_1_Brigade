cabb6230faed61eac04445d8c7c9c955a947d587
niki-gor <nic.gor@yandex.ru>
2023-05-19 16:33:20 +0300

    Modify test

--- internal/pkg/serialization/serialization_test.go
+++ internal/pkg/serialization/serialization_test.go
@@ -6,19 +6,26 @@ import (
 	"testing"
 
 	"github.com/labstack/echo/v4"
+	"github.com/mailru/easyjson"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMarshal(t *testing.T) {
 	e := echo.New()
 	e.JSONSerializer = EasyJsonSerializer{}
+
+	msg := Obj{Str: "ok"}
 	e.GET("/marshal", func(ctx echo.Context) error {
-		return ctx.JSON(http.StatusOK, Obj{Str: "ok"})
+		return ctx.JSON(http.StatusOK, msg)
 	})
+	
 	req := httptest.NewRequest(http.MethodGet, "/marshal", nil)
 	rec := httptest.NewRecorder()
 	e.ServeHTTP(rec, req)
 
 	require.Equal(t, rec.Code, http.StatusOK)
-	require.Equal(t, rec.Body.String(), "{\"str\":\"ok\"}")
+
+	received := Obj{}
+	easyjson.Unmarshal(rec.Body.Bytes(), &received)
+	require.Equal(t, received, msg)
 }

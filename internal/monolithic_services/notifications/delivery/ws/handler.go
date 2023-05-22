d4e88faf59cfbe754a47d5f477507565932a0ffb
marcussss1 <danilalakalash60@gmail.com>
2023-05-22 16:34:05 +0300

    edited notifications logic

--- internal/monolithic_services/notifications/delivery/ws/handler.go
+++ internal/monolithic_services/notifications/delivery/ws/handler.go
@@ -31,7 +31,7 @@ func (u *notificationsHandler) SendNotificationsHandler(ctx echo.Context) error 
 	sub, _ := u.centrifugo.GetSubscription(u.channelName)
 
 	sub.OnPublication(func(e centrifuge.PublicationEvent) {
-		session := ctx.Get("session").(model.Session)
+		//session := ctx.Get("session").(model.Session)
 
 		var producerMessage model.ProducerMessage
 		err := easyjson.Unmarshal(e.Data, &producerMessage)

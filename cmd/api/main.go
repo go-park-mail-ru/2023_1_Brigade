1076ff208863872036f21168735bd8989068c5b8
marcussss1 <danilalakalash60@gmail.com>
2023-05-24 13:59:03 +0300

    deleted csrf token

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/centrifugal/centrifuge-go"
-	"net/http"
 	"os"
 	"os/signal"
 	clientAuth "project/internal/microservices/auth/delivery/grpc/client"
@@ -245,22 +244,31 @@ func main() {
 		ExposeHeaders:    config.Cors.ExposeHeaders,
 	}))
 
-	DefaultCSRFConfig := middleware.CSRFConfig{
-		Skipper:        middleware.DefaultSkipper,
-		TokenLength:    32,
-		TokenLookup:    "header:" + echo.HeaderXCSRFToken,
-		ContextKey:     "csrf",
-		CookieName:     "_csrf",
-		CookieMaxAge:   86400,
-		CookieSecure:   true,
-		CookieHTTPOnly: true,
-	}
-	e.Use(middleware.CSRFWithConfig(DefaultCSRFConfig))
+	//DefaultCSRFConfig := middleware.CSRFConfig{
+	//	Skipper:        middleware.DefaultSkipper,
+	//	TokenLength:    32,
+	//	TokenLookup:    "header:" + echo.HeaderXCSRFToken,
+	//	ContextKey:     "csrf",
+	//	CookieName:     "_csrf",
+	//	CookieMaxAge:   86400,
+	//	CookieSecure:   true,
+	//	CookieHTTPOnly: true,
+	//}
+	//e.Use(middleware.CSRFWithConfig(DefaultCSRFConfig))
 
-	e.GET("api/v1/csrf/", func(ctx echo.Context) error {
-		ctx.Response().Header().Set("X-Csrf-Token", ctx.Get("csrf").(string))
-		return ctx.NoContent(http.StatusOK)
-	})
+	//e.GET("api/v1/csrf/", func(ctx echo.Context) error {
+	//	ctx.Response().Header().Set("X-Csrf-Token", ctx.Get("csrf").(string))
+	//	return ctx.NoContent(http.StatusOK)
+	//})
+
+	//e.POST("api/v1/csrf/", func(ctx echo.Context) error {
+	//	ctx.Response().Header().Set("X-Csrf-Token", ctx.Get("csrf").(string))
+	//	return ctx.NoContent(http.StatusOK)
+	//})
+	//e.GET("api/v1/csrf/", func(ctx echo.Context) error {
+	//	ctx.Response().Header().Set("X-Csrf-Token", ctx.Get("csrf").(string))
+	//	return ctx.NoContent(http.StatusOK)
+	//})
 
 	e.Use(myMiddleware.LoggerMiddleware)
 	e.Use(myMiddleware.AuthMiddleware(authSessionUsecase))

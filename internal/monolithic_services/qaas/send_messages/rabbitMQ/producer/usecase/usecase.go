57474ad0098cd53abf5a0e95d0266dde65a44d00
marcussss1 <danilalakalash60@gmail.com>
2023-05-30 12:13:12 +0300

    added dead letter exchange

--- internal/monolithic_services/qaas/send_messages/rabbitMQ/producer/usecase/usecase.go
+++ internal/monolithic_services/qaas/send_messages/rabbitMQ/producer/usecase/usecase.go
@@ -204,7 +204,7 @@ func NewProducer(connAddr string, queueName string) (producer.Usecase, error) {
 	err = channel.ExchangeDeclare(
 		"user_dlx",
 		"fanout",
-		false,
+		true,
 		false,
 		false,
 		false,
@@ -216,7 +216,7 @@ func NewProducer(connAddr string, queueName string) (producer.Usecase, error) {
 
 	_, err = channel.QueueDeclare(
 		"user_create_dlx",
-		false,
+		true,
 		false,
 		false,
 		false,

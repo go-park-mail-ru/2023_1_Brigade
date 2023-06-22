9d1e20ff380c444953b16f838f30eedca406ab6e
marcussss1 <danilalakalash60@gmail.com>
2023-06-22 23:09:22 +0300

    edited create users logic

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -5,8 +5,8 @@ DB_BRIGADE_CHATS_USER=$4
 DB_BRIGADE_CHATS_PASSWORD=$5
 su postgres <<EOF
 createdb  $DB_NAME;
-psql -U postgres -d  $DB_NAME -f ./sql/PostreSQLCreateMainUser.sql -v user_name='DB_BRIGADE_USER' -v user_password='DB_BRIGADE_PASSWORD'
+psql -U postgres -d  $DB_NAME -f ./db/grant_brigade_user.sql -v user_name='DB_BRIGADE_USER' -v user_password='DB_BRIGADE_PASSWORD'
 echo "Brigase user 'DB_BRIGADE_USER' created."
-psql -U postgres -d  $DB_NAME -f ./sql/PostreSQLCreateAuthUser.sql -v user_name='DB_BRIGADE_CHATS_USER' -v user_password='DB_BRIGADE_CHATS_PASSWORD'
+psql -U postgres -d  $DB_NAME -f ./db/grant_brigade_chats_user.sql -v user_name='DB_BRIGADE_CHATS_USER' -v user_password='DB_BRIGADE_CHATS_PASSWORD'
 echo "Brigade_chats user 'DB_BRIGADE_CHATS_PASSWORD' created."
 EOF
